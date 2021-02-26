594a263d4dc1d7f5bd3cc2ed9da16719a8616fc0
3817061Rezantsev <rezantsev.sergey@gmail.com>
2021-02-26 15:54:31 +0300

    model web controllers

---
+++ moderation-service/moderation-service-impl/src/main/java/com/netcracker/kinopoisk/moderation/impl/ModerationController.java
@@ -0,0 +1,10 @@
+package com.netcracker.kinopoisk.moderation.impl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/moderation")
+public class ModerationController {
+
+}
